c4783068519f113f022d0f215921e2e8e2355062
Takumi Maruyama <satouredstone@gmail.com>
2017-02-10 01:22:40 +0900

    More fast

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,15 +1,8 @@
 array=[9,6,7,1,2]
 i=0
 
-while i < len(array):#配列の順番を決める
-	j=0
-	while j < i:#前の配列と比べる
-		if array[i]<array[j]:
-			tmp=array[j]
-			array[j]=array[i]
-			array[i]=tmp
-		j=j+1
-		pass
-	i=i+1
-	pass
-print(array)
+for i in range(1,len(array)):#配列の順番を決める
+    for j in range(i):
+        if array[i]<array[j]:
+            array[j],array[i]=array[i],array[j]
+        print(array)
