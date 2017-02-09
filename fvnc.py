35749bee40da9f6b60539ff45ec4cc5d5ea18c50
Takumi Maruyama <satouredstone@gmail.com>
2017-02-10 00:39:54 +0900

    easy to read

--- fvnc.py
+++ fvnc.py
@@ -1,25 +1,18 @@
 #fvnc.py
-def sumfvnc(fvnc,count):
-	sum=0
-	i=0
-	while i <= 1:
-		sum+=fvnc[count-i]
-		i+=1
-	return sum
+def sumfvnc(fvnc):
+    number = fvnc[-1] + fvnc[-2]
+    return number
 
 
-
-
-number=int(input())
-fvnc=[]
-i=0
-while i<(number):
-	if i<=1:
-		fvnc.append(i)
-	else:
-		fvnc.append(sumfvnc(fvnc,i-1))
-	i+=1
-	pass
+number = int(input())
+fvnc = []
+i = 0
+for i in range(number):
+    if i <= 1:
+        fvnc.append(i)
+    else:
+        fvnc.append(sumfvnc(fvnc))
+    i += 1
 print(fvnc)
 
 
