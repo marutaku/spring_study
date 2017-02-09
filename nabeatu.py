e173de05b25059a3c50d91dc153c4220f21bdd58
Takumi Maruyama <satouredstone@gmail.com>
2017-02-10 01:34:35 +0900

    extend over 100

--- nabeatu.py
+++ nabeatu.py
@@ -1,13 +1,9 @@
 #nabeatu
-output=''
+output = ''
 for i in range(1,101):
-	if i%3==0:
-		output+="aho"
-	elif (i-3)%10==0:
-		output+="aho"
-	elif 30<=i<=39:
-		output+="aho"
-	else:
-		output+=str(i)
-	output+="\n"
+    if i % 3 == 0 or "3" in str(i):
+        output += "aho"
+    else:
+        output += str(i)
+    output += "\n"
 print(output)
