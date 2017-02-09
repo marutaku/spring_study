66d6fb62f1960239227da8bbe8c9390448d9120d
Takumi Maruyama <satouredstone@gmail.com>
2017-02-10 01:23:54 +0900

    Easy to read

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,8 +1,8 @@
-array=[9,6,7,1,2]
-i=0
+array = [9,6,7,1,2]
+i = 0
 
 for i in range(1,len(array)):#配列の順番を決める
     for j in range(i):
-        if array[i]<array[j]:
-            array[j],array[i]=array[i],array[j]
+        if array[i] < array[j]:
+            array[j],array[i] = array[i],array[j]
         print(array)
