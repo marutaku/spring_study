045412d7611deb1852e2ae226e4b4533326ec11e
unknown <>
2017-02-09 15:29:23 +0900

    fix bug

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,16 +1,20 @@
-array = [9,6,7,1,2]
-count=0
-
 def change (count,array):
 	tmp=array[count]
 	array[count]=array[count+1]
 	array[count+1]=tmp
 	return array
 
-while count<=(len(array)-2):
-	if array[count]>array[count+1]:
-		change(count,array)
-	count=count+1
-	print(array)
+array = [9,6,7,1,2]
+
+i=0
+
+while i<len(array):
+	count=0
+	while count<=(len(array)-2):
+		if array[count]>array[count+1]:
+			change(count,array)
+		count=count+1
+		pass
+	i=i+1
 	pass
-		
+print(array)
