115132fe120fb2431eb56df8fbc24678d5b7de56
unknown <>
2017-02-09 16:01:03 +0900

    rename sort2.py

---
+++ insertion_sort.py
@@ -0,0 +1,14 @@
+array=[9,6,7,1,2]
+i=0
+
+while i < len(array):#配列の順番を決める
+	j=0
+	while j < i:#前の配列と比べる
+		if array[i]<array[j]:
+			tmp=array[j]
+			array[j]=array[i]
+			array[i]=tmp
+		j=j+1
+		pass
+	i=i+1
+	pass
