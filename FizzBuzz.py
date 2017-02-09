914d1eb1f7306b1537742903eaecd69c451a96d7
Takumi Maruyama <satouredstone@gmail.com>
2017-02-10 01:27:12 +0900

    Easy to read

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,12 @@
-stdout=""
+stdout = ""
 for i in range(1,101):
-	if i%3 == 0 and i%5 == 0:
-		stdout+="FizzBuzz"
-	elif i%3 == 0:
-		stdout+="Fizz"
-	elif i%5 == 0:
-		stdout+='Buzz'
-	else:
-		stdout+=str(i)
-	stdout+="\n"
+    if i % 3 == 0 and i % 5 == 0:
+        stdout += "FizzBuzz"
+    elif i % 3 == 0:
+    	stdout += "Fizz"
+    elif i % 5 == 0:
+    	stdout += 'Buzz'
+    else:
+    	stdout += str(i)
+    stdout += "\n"
 print(stdout)
